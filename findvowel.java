a82b1ece42110b65f5b1869a23d8921245ebd8c8
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-01 17:36:32 +0530

    Create findvowel.java

---
+++ findvowel.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if((s.charAt(i)=='a') ||(s.charAt(i)=='e')||(s.charAt(i)=='i')||(s.charAt(i)=='o')||(s.charAt(i)=='u')) count++;
+        }
+        if(count==0) System.out.println("no");
+        else System.out.println("yes");
+           
+        
+        
+    }
+}
