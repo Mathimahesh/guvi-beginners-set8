9ac8a82852bc7b1601335e43232a1ea61ac9cb12
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-02 11:06:40 +0530

    Create floatToint.java

---
+++ floatToint.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextFloat();
+        double b = Math.ceil(a);
+        System.out.println(Math.round(b));
+        
+    }
+}
