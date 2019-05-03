6c08d79babdb831e15d3057b0d2ce9aecb6af8a5
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-03 19:19:12 +0530

    Update factors.java

--- factors.java
+++ factors.java
@@ -5,7 +5,7 @@ public class MyClass {
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                System.out.println(i+" ");
+                System.out.print(i+" ");
             }
         }
         
